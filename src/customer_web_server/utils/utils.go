1a4cdaac44ac94f40c3184233fd9497482dfe6cb
Andrew Lee <leeas@stolaf.edu>
2018-01-29 07:03:10 -0600

    ehh

---
+++ src/customer_web_server/utils/utils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// A function to generate a random 40 character string using time.Now as a seed to use as a secure token.
+func GenerateRandomHash() string {
+
+	b := make([]byte, 40)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	src := rand.NewSource(time.Now().UnixNano())
+	for i, cache, remain := 39, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
