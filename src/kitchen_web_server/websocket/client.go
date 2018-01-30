fd063e472d6bb639626e10d01d8b4c4cfdabb20f
Andrew Lee <leeas@stolaf.edu>
2018-01-29 23:45:08 -0600

    ehh

--- src/kitchen_web_server/websocket/client.go
+++ src/kitchen_web_server/websocket/client.go
@@ -26,6 +26,9 @@ type Message struct {
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func ServeClient(hub *Hub, w http.ResponseWriter, r *http.Request) {
